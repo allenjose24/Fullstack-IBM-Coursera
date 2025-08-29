977a8a049d351e9e6c97fd89326583072f62cd88
allenjose24 <allenjose2110@gmail.com>
2025-08-29 23:38:42 +0530

    Completed the addRecommendation Fxn

--- IntroToHTML_CSS_JS/Module-4-Final-Project/script.js
+++ IntroToHTML_CSS_JS/Module-4-Final-Project/script.js
@@ -1,22 +1,32 @@
 function addRecommendation() {
   // Get the message of the new recommendation
   let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
 
-    // Create a new 'recommendation' element and set it's value to the user's message
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() !== "") {
+    console.log("New recommendation added");
+
+    // Call showPopup here
+    showPopup(true);
+
+    // Create a new 'recommendation' element and set its value to the user's message
     var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.setAttribute("class", "recommendation");
+    element.innerHTML = "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
     // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
+    document.getElementById("all_recommendations").appendChild(element);
+
     // Reset the value of the textarea
     recommendation.value = "";
+
+    // (Optional) set focus back to the textarea
+    recommendation.focus();
+  } else {
+    // If empty, you could also show a small alert or popup
+    alert("Please enter a recommendation before submitting!");
   }
 }
+
 
 function showPopup(bool) {
   if (bool) {
